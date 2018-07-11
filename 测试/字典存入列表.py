18e6fd63f73f824d2505e6c4246c463de0553b13
lucycore <1296720721@qq.com>
2018-07-11 20:51:49 +0800

    更多想法

---
+++ 测试/字典存入列表.py
@@ -0,0 +1,22 @@
+
+user = {}
+
+zlb = []
+
+username = "lucycore"
+password = "killyou3000"
+time = "0000-00-00-00-00"
+Mac = "78:4f:43:98:c3:8c"
+
+zlb = [password,time,Mac]
+
+
+user[username] = zlb
+
+print(user)
+
+fw = user[username]
+
+print(fw)
+
+
