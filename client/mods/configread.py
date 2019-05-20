4d873ed1b226ad3c5749ac9f0f0fbea4a30a325c
lucycore <1296720721@qq.com>
2019-05-20 17:24:55 +0800

    Update configread.py

--- client/mods/configread.py
+++ client/mods/configread.py
@@ -108,7 +108,59 @@ def return_config():
 
 	}
 
-	test_mac = {}
+	
+	test_mac = {
+
+		"lcv2_server_install_url" : \
+		"http://www.lucycore.top/v2ray/lcv2Min.zip",
+		
+		"v2ray_server_install_url" : \
+		"http://www.lucycore.top/v2ray/v2rayMin.zip",
+		
+		"mkdir_v2ray_lj_1" : \
+		"./pythonz6/sun36x64",
+
+		"mkdir_v2ray_lj_2" : \
+		"./pythonz6/unsers",
+
+		"v2ray_zip_road" : \
+		"./pythonz6/sun36x64/V.zip",
+
+		"v2ray_zip_jy_road" : \
+		"./pythonz6/sun36x64/v2ray",
+
+		"v2rayN_json_url" : \
+		"http://www.lucycore.top/config/v2n_1.json",
+
+		"v2rayN_json_bd_lj" : \
+		"./pythonz6/sun36x64/v2ray/guiNConfig.json",
+
+		"v2ray_exe_strat_lj" : \
+		r'C:\pythonz6\sun36x64\v2ray\v2rayN.exe',
+
+		"lcv2_server_url" : \
+		'www.lucycore.top',
+
+		"server_port" : \
+		2233,
+
+		"lcv2_zip_bd_lj" : \
+		r'C:\pythonz6\lcv2.zip',
+		
+		"v2ray_bt_yz_list" : \
+		["v2ray.exe","v2rayN.exe","v2ctl.exe","geoip.dat",\
+		"geosite.dat","v2ctl.exe.sig","v2ray.exe.sig","wv2ray.exe"],
+
+		"v2ray_rm_all" : \
+		r'C:\pythonz6',
+
+		"lcv2_user_json" : \
+		r'C:\pythonz6\unsers\user.json',
+
+		"sys" : \
+		r'windows'
+
+	}
 
 
 	mac = {}
