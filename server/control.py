e5c9e9529ae7d6103a7e93bde42901ef1981b06c
lucycore <32613263+lucycore@users.noreply.github.com>
2019-06-22 14:48:25 +0800

    更新6.3服务器版本

--- server/control.py
+++ server/control.py
@@ -4,7 +4,6 @@ import socket
 
 
 def help():
-	print("\n\n")
 	print("-ak -------- 添加卡密")
 	print("-dk -------- 删除卡密")
 	print("-lk -------- 浏览卡密")
@@ -21,11 +20,14 @@ def add_key():
 	send_data = key_name + "!" + time
 
 	sock = socket.socket()
-	HOST = "www.lucycore.top"
-	#HOST = "35.229.171.103"
+	#HOST = "www.lucycore.top"
+	HOST = "192.168.0.11"
 	PORT = 2233
 	sock.connect((HOST, PORT))
 	#发送模式
+	sock.sendall("root".encode())
+	server_myd = sock.recv(1024).decode()
+
 	sock.sendall("add-key".encode())
 	server_myd = sock.recv(1024).decode()
 
@@ -43,11 +45,14 @@ def del_key():
 	send_data = key_name
 
 	sock = socket.socket()
-	HOST = "www.lucycore.top"
-	#HOST = "35.229.171.103"
+	#HOST = "www.lucycore.top"
+	HOST = "192.168.0.11"
 	PORT = 2233
 	sock.connect((HOST, PORT))
 	#发送模式
+	sock.sendall("root".encode())
+	server_myd = sock.recv(1024).decode()
+
 	sock.sendall("del-key".encode())
 	server_myd = sock.recv(1024).decode()
 
@@ -60,21 +65,142 @@ def del_key():
 
 
 
+def skim_key():
+	print("\n卡密查询模式\n")
+
+	sock = socket.socket()
+	#HOST = "www.lucycore.top"
+	HOST = "192.168.0.11"
+	PORT = 2233
+	sock.connect((HOST, PORT))
+	#发送模式
+	sock.sendall("root".encode())
+	server_myd = sock.recv(1024).decode()
+
+	sock.sendall("skim-key".encode())
+	server_key = sock.recv(1024).decode()
+
+	sock.close()
+
+	server_key=server_key.split('.')
+
+	for x in server_key:
+		print(x)
+
+
+def skim_user():
+	print("\n用户查询模式\n")
+
+	sock = socket.socket()
+	#HOST = "www.lucycore.top"
+	HOST = "192.168.0.11"
+	PORT = 2233
+	sock.connect((HOST, PORT))
+	#发送模式
+	sock.sendall("root".encode())
+	server_myd = sock.recv(1024).decode()
+
+	sock.sendall("skim-user".encode())
+	server_key = sock.recv(1024).decode()
+
+	sock.close()
+
+	server_key=server_key.split('.')
+
+	for x in server_key:
+		print(x)
+
+
+def skim_config():
+	print("\n配置查询模式\n")
+
+	sock = socket.socket()
+	#HOST = "www.lucycore.top"
+	HOST = "192.168.0.11"
+	PORT = 2233
+	sock.connect((HOST, PORT))
+	#发送模式
+	sock.sendall("root".encode())
+	server_myd = sock.recv(1024).decode()
+
+	sock.sendall("skim-config".encode())
+	server_key = sock.recv(1024).decode()
+
+	sock.close()
+
+	server_key=server_key.split('.')
+
+	for x in server_key:
+		print(x)
+
+
+def del_user():
+	print("\n用户删除模式\n")
+	key_name = input("user_name:")
+	send_data = key_name
+
+	sock = socket.socket()
+	#HOST = "www.lucycore.top"
+	HOST = "192.168.0.11"
+	PORT = 2233
+	sock.connect((HOST, PORT))
+	#发送模式
+	sock.sendall("root".encode())
+	server_myd = sock.recv(1024).decode()
+
+	sock.sendall("del-user".encode())
+	server_myd = sock.recv(1024).decode()
+
+	sock.sendall(send_data.encode())
+
+	fk = sock.recv(1024).decode()
+
+	sock.close()
+	print("执行完成，服务器反馈：" + fk)
+	
+
+def del_config():
+	print("\n配置删除模式\n")
+	key_name = input("ip:")
+	send_data = key_name
+
+	sock = socket.socket()
+	#HOST = "www.lucycore.top"
+	HOST = "192.168.0.11"
+	PORT = 2233
+	sock.connect((HOST, PORT))
+	#发送模式
+	sock.sendall("root".encode())
+	server_myd = sock.recv(1024).decode()
+
+	sock.sendall("del-config".encode())
+	server_myd = sock.recv(1024).decode()
+
+	sock.sendall(send_data.encode())
+
+	fk = sock.recv(1024).decode()
+
+	sock.close()
+	print("执行完成，服务器反馈：" + fk)
+	
+
 def mod_c(cs):
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
-	if cs == "":
+	if cs == "-ak":
+		add_key()
+	if cs == "-dk":
+		del_key()
+	if cs == "-h":
+		help()
+	if cs == "-lk":
+		skim_key()
+	if cs == "-lu":
+		skim_user()
+	if cs == "-du":
+		del_user()
+	if cs == "-lc":
+		skim_config()
+	if cs == "-dc":
+		del_config()
 
 
 def main():
@@ -85,4 +211,4 @@ def main():
 		cmd = input("lcv2: ")
 		mod_c(cmd)
 
-
+main()
