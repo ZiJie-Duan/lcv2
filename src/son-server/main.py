576fc320b947b17b90d9dda6dbb32ec644ccc0a3
lucycore <32613263+lucycore@users.noreply.github.com>
2019-12-01 08:36:10 +0800

    子服务器操作函数更新

--- src/son-server/main.py
+++ src/son-server/main.py
@@ -1,10 +1,17 @@
 import subprocess
-
+import uuid
 
 class Lcv2_api_core():
-	"""docstring for Lcv2_api_core"""
+	"""docstring for Lcv2_api_core
+	用于操作v2ray api工具的子服务器控制模块
+	包含有以下 api 功能：
+		添加用户
+		删除用户
+		读取用户流量使用情况
+	以上所有功能的实现必须提供邮箱与uuid
+	"""
 
-	def __init__(self,mod,email,uuid):
+	def __init__(self,mod="",email="",uuid=""):
 		self.mod = mod
 		self.email = email
 		self.uuid = uuid
@@ -14,26 +21,69 @@ class Lcv2_api_core():
 		self.level = "1"
 		self.alterld = "10"
 
-	def api(self):
+
+	def api_run(self):
 
 		command = "./v2rayapi "+self.mod+" " +\
 		self.email+" "+self.uuid+" "+self.api_address\
 		+" "+self.api_port+" "+self.inbound_tag\
 		+" "+self.level+" "+self.alterld
-
+		print(command)
 		back = subprocess.Popen(command, shell=True, \
 			stdout=subprocess.PIPE,stderr=\
 			subprocess.PIPE).communicate()
 
+		alist = []
 		for x in back:
-			print(x.decode())
+			alist.append(x.decode())
+
+		return alist
 
 
-class ClassName(object):
-	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
+	def api_read(self):
+
+		command = "/usr/bin/v2ray/v2ctl api --server=" + self.api_address\
+		+":"+self.api_port+""" StatsService.GetStats 'name: """\
+		+'"user>>>'+self.email+'>>>traffic>>>downlink" '\
+		+""" reset: false'"""
+		print(command)
+		back = subprocess.Popen(command, shell=True, \
+			stdout=subprocess.PIPE,stderr=\
+			subprocess.PIPE).communicate()
+
+		alist = []
+		for x in back:
+			alist.append(x.decode())
+
+		return alist
+
+
+def main():
+	#mod 参数为 read / del / add
+
+	mod = input("mod>>")
+	email = input("email>>")
+	uuidn = str(uuid.uuid4())
+	api = Lcv2_api_core()
+
+	if mod == "read":
+		api.email = email
+		back = api.api_read()
+		print(back)
+
+	elif mod == "del":
+		api.mod = "del"
+		api.uuid = uuidn
+		api.email = email
+		back = api.api_run()
+		print(back)
+
+	elif mod == "add":
+		api.mod = "add"
+		api.uuid = uuidn
+		api.email = email
+		back = api.api_run()
+		print(back)
+
 		
-		
-		
+main()
